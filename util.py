7faea84e4966290447114443962704f65cb2c179
rashnov <rashnov@users.noreply.github.com>
2022-01-09 21:56:59 +1300

    Update util.py

--- util.py
+++ util.py
@@ -13,17 +13,5 @@ def download_file(x, y, z):
 
 cookie = open('cookie.txt').readline()
 headers = CaseInsensitiveDict()
-headers["authority"] = "echo360.net.au"
-headers["sec-ch-ua"] = '" Not A;Brand";v="99", "Chromium";v="96", "Microsoft Edge";v="96"'
-headers["sec-ch-ua-mobile"] = "?0"
-headers["sec-ch-ua-platform"] = '"Windows"'
-headers["upgrade-insecure-requests"] = "1"
 headers["user-agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36 Edg/96.0.1054.53"
-headers["accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
-headers["sec-fetch-site"] = "same-origin"
-headers["sec-fetch-mode"] = "navigate"
-headers["sec-fetch-user"] = "?1"
-headers["sec-fetch-dest"] = "document"
-headers["referer"] = "https://echo360.net.au/section/"
-headers["accept-language"] = "en-GB,en;q=0.9"
 headers["cookie"] = cookie
